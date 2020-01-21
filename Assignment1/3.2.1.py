d543034324fc29690d6217a77882f8cfc7714a7e
fernando2393 <garciasanzfernando2393@gmail.com>
2020-01-21 16:28:19 +0100

    3.2.1 almost done. Plotting missing

--- Assignment1/3.2.1.py
+++ Assignment1/3.2.1.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import DataGeneration as dg
+import Algorithms as alg
 
+SAMPLES = 100
 
 mean = np.array([0,1])
 sigma = 1
@@ -12,52 +14,6 @@ v = np.random.rand(t.shape[0], n_hidden)
 w = np.random.rand(n_hidden, x.shape[0])
 dv = np.random.rand(t.shape[0], n_hidden)
 dw = np.random.rand(n_hidden, x.shape[0])
-
-
-def phi(x):
-    phi = 2/(1+np.exp(-x)) - 1
-    phi_derivative = (1+phi)*(1-phi)/2
-    return phi, phi_derivative
-
-def forward_pass(x, w, v):
-    hin = np.dot(w, x)
-    phi_h = phi(hin)[0]
-    hout =  np.vstack((phi_h))
-    
-    oin = np.dot(v, hout)
-    oout = phi(oin)[0]
-    
-    return hout, oout
-    
-def backward_pass(t, w, v, o, h):    
-    delta_o = (o - t) * ((1 + o) * (1 - o)) * 0.5
-    ok = 1
-    delta_h = np.dot(np.transpose(v), delta_o) * ((1 + h) * (1 - h))*0.5
-    delta_h = delta_h[range(delta_h.shape[0]),:]    #remove extra rows
-    
-    return delta_o, delta_h
-
-def weight_update(x, dw, dv, alpha, delta_h, delta_o, h, w, v):
-    dw = (dw * alpha) - np.dot(delta_o, np.transpose(x))*(1-alpha)
-    dv = (dv * alpha) - np.dot(delta_h, np.transpose(h))*(1-alpha)
-    W = w + dw*eta
-    V = v + dv*eta
-    return W, V
-
-
-#print("This is x", x)
-#print("\nThis is t", t)
-print(t.shape)
-
-
-#print("\nThis is phi of x", phi(x))
-
-
-h, o = forward_pass(x, w, v)
-delta_o, delta_h = backward_pass(t,w,v,o,h)
-
-alpha = 0.9
-eta = 0.001
-
-print(weight_update(x, dw, dv,alpha, delta_h, delta_o,h, w, v))
+x_grid = np.arange(min(x[0,:]), max(x[0,:]), (max(x[0,:]) - min(x[0,:])) / SAMPLES)
+alg.plot_boundary_multilayer(x, w, v, dw, dv, t, x_grid)
 
--- Assignment1/Algorithms.py
+++ Assignment1/Algorithms.py
@@ -7,6 +7,7 @@ ETA = 0.001
 D_BATCH = 1
 D_SEQUENTIAL = 2
 PERCEPTRON = 3
+ALPHA = 0.9
 
 def delta_rule_batch(x, t, w):
     w_aux = w.copy()
@@ -17,7 +18,7 @@ def delta_rule_batch(x, t, w):
 def delta_rule_sequential(x, t, w):
     delta_w = w.copy()
     for i in range(x.shape[1]):
-        delta_w = delta_w - ETA * np.dot(np.dot(w, x[:, i, None]) - t[:, i], np.transpose(x[:, i, None]))
+        delta_w = delta_w - ETA * np.dot(np.dot(delta_w, x[:, i, None]) - t[:, i], np.transpose(x[:, i, None]))
         
     return delta_w
     
@@ -89,3 +90,51 @@ def run_algorithms(x_grid, x, t, w):
 
     # Case: Simple Perceptron
     plot_boundary(x_grid, x, t, w, PERCEPTRON)
+
+# ---------------------------- MULTILAYER PERCEPTRON ---------------------------- #
+
+def phi(x):
+    phi = 2 / (1 + np.exp(-x)) - 1
+    return phi
+
+def forward_pass(x, w, v):
+    h_in = np.dot(w, x)
+    phi_h = phi(h_in)
+    h_out =  np.vstack((phi_h))
+    o_in = np.dot(v, h_out)
+    o_out = phi(o_in)
+    
+    return h_out, o_out
+    
+def backward_pass(t, w, v, o, h):    
+    delta_o = (o - t) * ((1 + o) * (1 - o)) * 0.5
+    ok = 1
+    delta_h = np.dot(np.transpose(v), delta_o) * ((1 + h) * (1 - h)) * 0.5
+    delta_h = delta_h[range(delta_h.shape[0]),:]    #remove extra rows
+    
+    return delta_o, delta_h
+
+def weight_update(x, dw, dv, delta_h, delta_o, h, w, v):
+    dw = (dw * ALPHA) - np.dot(delta_o, np.transpose(x)) * (1 - ALPHA)
+    dv = (dv * ALPHA) - np.dot(delta_h, np.transpose(h)) * (1 - ALPHA)
+    W = w + dw * ETA
+    V = v + dv * ETA
+    return W, V
+
+def plot_boundary_multilayer(x, w, v, dw, dv, t, x_grid):
+    for i in range(ITERATIONS):
+        h, o = forward_pass(x, w, v)
+        delta_o, delta_h = backward_pass(t, w, v, o, h)
+        w, v = weight_update(x, dw, dv, delta_h, delta_o, h, w, v)
+        z = o
+        plt.clf()
+        axes = plt.gca()
+        axes.set_xlim(min(x[0,:]), max(x[0,:]))
+        axes.set_ylim(min(x[1,:]), max(x[1,:]))
+        plt.contourf(x_grid, x_grid.reshape(-1, 1), z, levels=1)
+        plt.scatter(x[0,:], x[1,:], c=t[0,:])
+        if (i == 0 or i == ITERATIONS - 1):
+            plt.show(block=True)
+        else:
+            plt.show(block=False)
+            plt.pause(0.005)
