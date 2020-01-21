Plotting in 3.2.1 (wrong results). Removed values in data generation are now saved and retrieved

Assignment1/3.1.3.py
@@ -12,7 +12,7 @@ D_SEQUENTIAL = 2
 PERCEPTRON = 3
 
 def main():
-
+    
     # -------------------- FIRST PART -> Linear Separation for non-linearly separable data -------------------- #
     m = [1.0, 0.5]
     sigma = 0.5
@@ -23,13 +23,14 @@ def main():
     alg.run_algorithms(x_grid, x, t, w)
 
     # -------------------- SECOND PART -> Separation of non-linearly separable data -------------------- #
-
+    
     # Case: Original new data
     m_a = [1.0, 0.3]
     m_b = [0.0, -0.1]
     sigma_a = 0.2
     sigma_b = 0.3
     x, t = dg.new_data_generation(m_a, m_b, sigma_a, sigma_b)
+
     x_grid = np.arange(min(x[0,:]), max(x[0,:]), (max(x[0,:]) - min(x[0,:])) / SAMPLES)
     w = np.random.rand(t.shape[0], x.shape[0])
     print("Running algorithms for: New generated non-linearly separable data")
@@ -38,7 +39,7 @@ def main():
     # Case: Remove 25% from each class
     x_temp = x.copy()
     t_temp = t.copy()
-    x_temp, t_temp = dg.generate_training_a_b(x_temp, t_temp, 0.25)
+    x_temp, t_temp, x_test, t_test = dg.generate_training_a_b(x_temp, t_temp, 0.25)
     x_grid = np.arange(min(x_temp[0,:]), max(x_temp[0,:]), (max(x_temp[0,:]) - min(x_temp[0,:])) / SAMPLES)
     # w can be equal, since t.shape[0] and x.shape[0] will not change (only column removal)
     print("Running algorithms for: New generated non-linearly separable data --> 25% removed in each class")
@@ -47,7 +48,7 @@ def main():
     # Case: Remove 50% from class A
     x_temp = x.copy()
     t_temp = t.copy()
-    x_temp, t_temp = dg.generate_training_a(x_temp, t_temp, 0.5)
+    x_temp, t_temp, x_test, t_test = dg.generate_training_a(x_temp, t_temp, 0.5)
     x_grid = np.arange(min(x_temp[0,:]), max(x_temp[0,:]), (max(x_temp[0,:]) - min(x_temp[0,:])) / SAMPLES)
     # w can be equal, since t.shape[0] and x.shape[0] will not change (only column removal)
     print("Running algorithms for: New generated non-linearly separable data --> 50% removed in class A")
@@ -56,7 +57,7 @@ def main():
     # Case: Remove 50% from class B
     x_temp = x.copy()
     t_temp = t.copy()
-    x_temp, t_temp = dg.generate_training_b(x_temp, t_temp, 0.5)
+    x_temp, t_temp, x_test, t_test = dg.generate_training_b(x_temp, t_temp, 0.5)
     x_grid = np.arange(min(x_temp[0,:]), max(x_temp[0,:]), (max(x_temp[0,:]) - min(x_temp[0,:])) / SAMPLES)
     # w can be equal, since t.shape[0] and x.shape[0] will not change (only column removal)
     print("Running algorithms for: New generated non-linearly separable data --> 50% removed in class B")
@@ -65,7 +66,7 @@ def main():
     # Case: Remove 20% from negative subset class A, 80% from positive subset class B
     x_temp = x.copy()
     t_temp = t.copy()
-    x_temp, t_temp = dg.generate_training_a_subsets(x_temp, t_temp, 0.2, 0.8)
+    x_temp, t_temp, x_test, t_test = dg.generate_training_a_subsets(x_temp, t_temp, 0.2, 0.8)
     x_grid = np.arange(min(x_temp[0,:]), max(x_temp[0,:]), (max(x_temp[0,:]) - min(x_temp[0,:])) / SAMPLES)
     # w can be equal, since t.shape[0] and x.shape[0] will not change (only column removal)
     print("Running algorithms for: New generated non-linearly separable data --> subsets removed in class A")

Assignment1/3.2.1.py
@@ -9,11 +9,13 @@ SAMPLES = 100
 mean = np.array([0,1])
 sigma = 1
 n_hidden = 5
-x, t = dg.non_linearly_separable_data(mean, sigma)
+#x, t = dg.linearly_separable_data([1.0, 0.5], 0.5, [-1.0, 0], 0.5)
+x, t = dg.new_data_generation([1.0, 0.3], [0, -0.1], 0.2, 0.3)
 v = np.random.rand(t.shape[0], n_hidden)
 w = np.random.rand(n_hidden, x.shape[0])
 dv = np.random.rand(t.shape[0], n_hidden)
 dw = np.random.rand(n_hidden, x.shape[0])
-x_grid = np.arange(min(x[0,:]), max(x[0,:]), (max(x[0,:]) - min(x[0,:])) / SAMPLES)
-alg.plot_boundary_multilayer(x, w, v, dw, dv, t, x_grid)
+x_grid, y_grid = np.meshgrid(np.arange(min(x[0,:]), max(x[0,:]), 
+(max(x[0,:]) - min(x[0,:])) / SAMPLES), np.arange(min(x[1,:]), max(x[1,:]), (max(x[1,:]) - min(x[1,:])) / SAMPLES))
+alg.plot_boundary_multilayer(x, w, v, dw, dv, t, x_grid, y_grid)
 

Assignment1/Algorithms.py
@@ -108,30 +108,36 @@ def forward_pass(x, w, v):
     
 def backward_pass(t, w, v, o, h):    
     delta_o = (o - t) * ((1 + o) * (1 - o)) * 0.5
-    ok = 1
     delta_h = np.dot(np.transpose(v), delta_o) * ((1 + h) * (1 - h)) * 0.5
     delta_h = delta_h[range(delta_h.shape[0]),:]    #remove extra rows
     
     return delta_o, delta_h
 
 def weight_update(x, dw, dv, delta_h, delta_o, h, w, v):
-    dw = (dw * ALPHA) - np.dot(delta_o, np.transpose(x)) * (1 - ALPHA)
-    dv = (dv * ALPHA) - np.dot(delta_h, np.transpose(h)) * (1 - ALPHA)
+    dw = (dw * ALPHA) - np.dot(delta_h, np.transpose(x)) * (1 - ALPHA)
+    dv = (dv * ALPHA) - np.dot(delta_o, np.transpose(h)) * (1 - ALPHA)
     W = w + dw * ETA
     V = v + dv * ETA
     return W, V
 
-def plot_boundary_multilayer(x, w, v, dw, dv, t, x_grid):
+def plot_boundary_multilayer(x, w, v, dw, dv, t, x_grid, y_grid):
     for i in range(ITERATIONS):
         h, o = forward_pass(x, w, v)
         delta_o, delta_h = backward_pass(t, w, v, o, h)
         w, v = weight_update(x, dw, dv, delta_h, delta_o, h, w, v)
-        z = o
+        x_input = np.c_[x_grid.ravel(), y_grid.ravel()]
+        x_input = np.transpose(x_input)
+        x_input = np.vstack((x_input, np.ones((x_input.shape[1]))))
+        z = forward_pass(x_input, w, v)[1]
+        z = z.reshape(x_grid.shape)
         plt.clf()
         axes = plt.gca()
         axes.set_xlim(min(x[0,:]), max(x[0,:]))
         axes.set_ylim(min(x[1,:]), max(x[1,:]))
-        plt.contourf(x_grid, x_grid.reshape(-1, 1), z, levels=1)
+        plt.contour(x_grid, y_grid, z, levels=[0])
+        plt.title("Multilayer Perceptron: Iteration %i" %(i+1))
+        plt.xlabel("$\mathregular{X_1}$ coordinate")
+        plt.ylabel("$\mathregular{X_2}$ coordinate")
         plt.scatter(x[0,:], x[1,:], c=t[0,:])
         if (i == 0 or i == ITERATIONS - 1):
             plt.show(block=True)

Assignment1/DataGeneration.py
@@ -92,6 +92,8 @@ Data removal condition:
 def generate_training_a(x, t, percentage):
     x_training = x
     t_training = t
+    x_test = list()
+    t_test = list()
 
     # Class A
     removal_pos_a = list()
@@ -100,11 +102,13 @@ def generate_training_a(x, t, percentage):
         if (t_training[0,aux] == 1):
             if (aux not in removal_pos_a):
                 removal_pos_a.append(aux)
+                x_test.append(x_training[:,aux])
+                t_test.append(t_training[:,aux])
 
     x_training = np.delete(x_training, removal_pos_a, axis=1)
     t_training = np.delete(t_training, removal_pos_a, axis=1)
     
-    return x_training, t_training
+    return x_training, t_training, np.transpose(np.array(x_test)), np.transpose(np.array(t_test))
 
 """
 Data removal condition:
@@ -113,6 +117,8 @@ Data removal condition:
 def generate_training_b(x, t, percentage):
     x_training = x
     t_training = t
+    x_test = list()
+    t_test = list()
 
     # Class B
     removal_pos_b = list()
@@ -121,11 +127,13 @@ def generate_training_b(x, t, percentage):
         if (t_training[0,aux] == -1):
             if (aux not in removal_pos_b):
                 removal_pos_b.append(aux)
+                x_test.append(x_training[:,aux])
+                t_test.append(t_training[:,aux])
 
     x_training = np.delete(x_training, removal_pos_b, axis=1)
     t_training = np.delete(t_training, removal_pos_b, axis=1)
     
-    return x_training, t_training
+    return x_training, t_training, np.transpose(np.array(x_test)), np.transpose(np.array(t_test))
 
 """
 Data removal condition:
@@ -134,10 +142,10 @@ Data removal condition:
 def generate_training_a_b(x, t, percentage):
     x_training = x
     t_training = t
-    x_training, t_training = generate_training_a(x_training, t_training, percentage)
-    x_training, t_training = generate_training_b(x_training, t_training, percentage)
+    x_training, t_training, x_test_1, t_test_1 = generate_training_a(x_training, t_training, percentage)
+    x_training, t_training, x_test_2, t_test_2 = generate_training_b(x_training, t_training, percentage)    
     
-    return x_training, t_training
+    return x_training, t_training, np.concatenate((x_test_1, x_test_2), axis=1), np.concatenate((t_test_1, t_test_2), axis=1)
 
 """
 Data removal condition:
@@ -147,6 +155,8 @@ Data removal condition:
 def generate_training_a_subsets(x, t, percentage1, percentage2):
     x_training = x
     t_training = t
+    x_test = list()
+    t_test = list()
 
     # Removing negative values
 
@@ -160,6 +170,8 @@ def generate_training_a_subsets(x, t, percentage1, percentage2):
     for i in range(cnt_neg):
         pos = randrange(len(cnt)) # Get random position in the array
         removal.append(cnt[pos]) # Append the value stored in the position
+        x_test.append(x_training[:,cnt[pos]])
+        t_test.append(t_training[:,cnt[pos]])
         cnt = np.delete(cnt, pos) # Delete appended value
 
     x_training = np.delete(x_training, removal, axis=1)
@@ -177,9 +189,11 @@ def generate_training_a_subsets(x, t, percentage1, percentage2):
     for i in range(cnt_pos):
         pos = randrange(len(cnt)) # Get random position in the array
         removal.append(cnt[pos]) # Append the value stored in the position
+        x_test.append(x_training[:,cnt[pos]])
+        t_test.append(t_training[:,cnt[pos]])
         cnt = np.delete(cnt, pos) # Delete appended value
 
     x_training = np.delete(x_training, removal, axis=1)
     t_training = np.delete(t_training, removal, axis=1)
 
-    return x_training, t_training
+    return x_training, t_training, np.transpose(np.array(x_test)), np.transpose(np.array(t_test))