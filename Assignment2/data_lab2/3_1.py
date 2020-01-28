70b8473cbec800e8c3c3f87eb26d2dcba0aadb44
Maria Bjelikj <bjelikm@gmail.com>
2020-01-28 15:09:25 +0100

    added thresholds

--- Assignment2/data_lab2/3_1.py
+++ Assignment2/data_lab2/3_1.py
@@ -36,31 +36,44 @@ def train_batch(x, y, mean, variance):
     
     return w
 
+def predict(x, y, mean, variance, w):
+    phi = np.zeros((x.shape[0], mean.shape[0]))
+    for i in range(x.shape[0]):
+        for j in range(mean.shape[0]):
+            phi[i][j] = Phi(x[i], mean[j], variance)
     
+    return np.dot(phi, w)
 
 def main():
     
-    rbf_nodes = 20
-    mean = np.linspace(0, 2 * np.pi, rbf_nodes)
-    variance = 1
-    
+    error_thresholds = [0.1, 0.01, 0.001]
     x_train, y_train, x_test, y_test = generate_data("sin2x")
-    w = train_batch(x_train, y_train, mean, variance)
     
-    phi = np.zeros((x_test.shape[0], mean.shape[0]))
-    for i in range(x_test.shape[0]):
-        for j in range(mean.shape[0]):
-            phi[i][j] = Phi(x_test[i], mean[j], variance)
+    rbf_nodes = 20
     
-    y_predicted = np.dot(phi, w)
-    
-    MSE = mse(y_test, y_predicted)
-    print("The MSE is: {}".format(MSE))
-    
-    MAE = mae(y_test, y_predicted)
-    print("The mean absolute error is: {}".format(MAE))
-    
-    
+    for i in range(5, rbf_nodes):
+        mean = np.linspace(0, 2 * np.pi, rbf_nodes)
+        variance = 1
+        
+        w = train_batch(x_train, y_train, mean, variance)
+        
+        phi = np.zeros((x_test.shape[0], mean.shape[0]))
+        for i in range(x_test.shape[0]):
+            for j in range(mean.shape[0]):
+                phi[i][j] = Phi(x_test[i], mean[j], variance)
+        
+        y_predicted = predict(x_test, y_test, mean, variance, w)
+        
+        MSE = mse(y_test, y_predicted)
+        #print("The MSE is: {}".format(MSE))
+        
+        MAE = mae(y_test, y_predicted)
+        #print("The mean absolute error is: {}".format(MAE))
+        
+        if len(error_thresholds) and MAE < max(error_thresholds):
+            # Threshold fulfilled, remove
+            error_thresholds.remove(max(error_thresholds))
+            
     plt.plot(x_train, y_train, label='real output')
     plt.plot(x_test, y_predicted, 'r--', label='prediction')
     plt.legend()
