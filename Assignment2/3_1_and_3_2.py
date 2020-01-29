7deadadb9675c3d5121dbb3a490b02fb06e731a0
fernando2393 <garciasanzfernando2393@gmail.com>
2020-01-29 10:44:03 +0100

    Code reformat

--- Assignment2/3_1_and_3_2.py
+++ Assignment2/3_1_and_3_2.py
@@ -6,10 +6,11 @@ from sklearn.metrics import mean_absolute_error as mae
 EPOCHS = 100
 ETA = 0.1
 
+
 def generate_data(f_type, noise=False):
     # For training data the range is 0 to 2pi
     x_train = np.arange(0, 2 * np.pi, 0.1)[:, np.newaxis]
-    
+
     # Test data starts from 0.05
     x_test = np.arange(0.05, 2 * np.pi, 0.1)[:, np.newaxis]
     y_train = 0
@@ -24,7 +25,7 @@ def generate_data(f_type, noise=False):
     if f_type == "square":
         y_train = np.where(np.sin(2 * x_train) >= 0, 1, -1)
         y_test = np.where(np.sin(2 * x_test) >= 0, 1, -1)
-        
+
     # Add noise for 3_2, zero mean data with variance 0.1
     if noise:
         y_train = y_train + np.random.normal(0, 0.1, x_train.shape[1])
@@ -55,68 +56,71 @@ def train_batch(x, y, mean, variance):
 
     return w
 
+
 def train_online_delta_rule(x, y, mean, variance):
     phi = phi_matrix(x, mean, variance)
-    w = np.random.rand(1, phi.shape[1]) # random initialisation for weights
-    MSE = []
+    w = np.random.rand(1, phi.shape[1])  # random initialisation for weights
+    mse_list = []
 
     for epoch in range(EPOCHS):
         for i in range(phi.shape[0]):
-            phi_i = np.reshape(phi[i], (1, len(phi[i]))) # just to avoid dimension error
-            error = y[i] - np.dot(phi_i, w.T) # error from real value to predicted
-            delta_w = ETA * np.dot(phi_i.T, error) # equation 10
+            phi_i = np.reshape(phi[i], (1, len(phi[i])))  # just to avoid dimension error
+            error = y[i] - np.dot(phi_i, w.T)  # error from real value to predicted
+            delta_w = ETA * np.dot(phi_i.T, error)  # equation 10
             w += delta_w.T
 
         y_predicted = np.dot(phi, w.T)
 
-        MSE_ = mse(y, y_predicted)
+        mse_ = mse(y, y_predicted)
 
         if epoch > 1:
-            MSE.append(MSE_)
+            mse_list.append(mse_)
 
     # Plot MSE vs Epochs
-    plt.plot(range(2, EPOCHS), MSE)
+    plt.plot(range(2, EPOCHS), mse_list)
     plt.xlabel('Epochs')
     plt.ylabel('MSE')
     plt.show()
-    
+
     return w
 
-def predict(x, y, mean, variance, w):
+
+def predict(x, mean, variance, w):
     phi = phi_matrix(x, mean, variance)
-    
+
     # The prediction is done multiplying RBF phi with the calculated weights w
     return np.dot(phi, w)
 
+
 def predict_square(x, y, mean, variance, w):
     phi = phi_matrix(x, mean, variance)
-    
+
     prediction = np.ones(y.shape)
     prediction[np.where(np.dot(phi, w) >= 0)] = 1
-    prediction[np.where(np.dot(phi, w)  < 0)] = -1
-    
+    prediction[np.where(np.dot(phi, w) < 0)] = -1
+
     return prediction
 
 
-
 def main():
-    
-    ################### 3.1: Training in Batch ######################
+    #  3.1: Training in Batch  #
     error_thresholds = [0.1, 0.01, 0.001]
     f_type = "sin2x"
     x_train, y_train, x_test, y_test = generate_data(f_type, False)
     y_predicted = 0
     rbf_nodes = 20
-    
+
     # Test error thresholds for various numbers of RBF nodes
     for i in range(5, rbf_nodes):
         mean = np.linspace(0, 2 * np.pi, i)
         variance = 1
         w = train_batch(x_train, y_train, mean, variance)
-        
-        if f_type == "sin2x": y_predicted = predict(x_test, y_test, mean, variance, w)
-        else: y_predicted = predict_square(x_test, y_test, mean, variance, w)
-        
+
+        if f_type == "sin2x":
+            y_predicted = predict(x_test, y_test, mean, variance, w)
+        else:
+            y_predicted = predict_square(x_test, y_test, mean, variance, w)
+
         mean_square_error = mse(y_test, y_predicted)
         # print("The MSE is: {}".format(MSE))
         mean_absolute_error = mae(y_test, y_predicted)
@@ -133,26 +137,28 @@ def main():
     plt.title("Training in batch mode")
     plt.legend()
     plt.show()
-    
-    ################### 3.2: Online Delta Rule #######################
+
+    #  3.2: Online Delta Rule  #
     f_type = "sin2x"
     x_train, y_train, x_test, y_test = generate_data(f_type, True)
     y_predicted = 0
     rbf_nodes = 50
-    
+
     mean = np.linspace(0, 2 * np.pi, rbf_nodes)
     variance = 1
-    
+
     w = train_online_delta_rule(x_train, y_train, mean, variance)
-    
-    if f_type == "sin2x": y_predicted = predict(x_test, y_test, mean, variance, w.T)
-    else: y_predicted = predict_square(x_test, y_test, mean, variance, w.T)
-    
+
+    if f_type == "sin2x":
+        y_predicted = predict(x_test, mean, variance, w.T)
+    else:
+        y_predicted = predict_square(x_test, y_test, mean, variance, w.T)
+
     mean_square_error = mse(y_test, y_predicted)
     # print("The MSE is: {}".format(MSE))
     mean_absolute_error = mae(y_test, y_predicted)
     # print("The mean absolute error is: {}".format(MAE))
-    
+
     plt.plot(x_train, y_train, label='real output')
     plt.plot(x_test, y_predicted, 'r--', label='prediction')
     plt.title("Online Delta Rule")
