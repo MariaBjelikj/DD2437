512a7a37a3a8a06d40449823350687a8b7481567
fernando2393 <garciasanzfernando2393@gmail.com>
2020-01-30 11:24:46 +0100

    Assignment 4.2 finished

--- Assignment2/Algorithms_part2.py
+++ Assignment2/Algorithms_part2.py
@@ -7,13 +7,12 @@ EPOCH = 20
 NEIGHBOR_DISTANCE_ANIMAL = 25
 NEIGHBOR_DISTANCE_CYCLING = 2
 
-
-
 """
 ##############################################################################
 # Functions for both tasks
 ###############################################################################
 """
+
 
 def read_data(filename, task):
     if task == "task4_1":
@@ -26,14 +25,14 @@ def read_data(filename, task):
         animals_dat = np.array(animals_dat).reshape((len(animal_names), len(animals_att)))
 
         return animal_names, animals_dat, animals_att
-    
+
     if task == "task4_2":
         data = pd.read_csv(filename[0], header=None)
         data[1].replace(regex=True, inplace=True, to_replace=r';', value=r'')
         cities = data.to_numpy()
         cities[:, 0] = [float(i) for i in cities[:, 0]]
         cities[:, 1] = [float(i) for i in cities[:, 1]]
-        
+
         return cities, data
 
 
@@ -68,16 +67,16 @@ def get_indeces_1dim(winner_position, length, neighbor_distance):
     return negative_index, positive_index
 
 
-def get_indeces_circular(winner_position, length, neighbor_distance):
+def get_indeces_circular(winner_position, neighbor_distance):
     negative_index = (winner_position - neighbor_distance)
     positive_index = (winner_position + neighbor_distance)
 
     return negative_index, positive_index
 
 
-def get_indeces_2dim(winner_position, length, neighbor_distance):
+def get_indeces_2dim(winner_position, neighbor_distance):
     pos = winner_position
-    negative_index_row, positive_index_row = get_indeces_circular(pos, length, neighbor_distance)
+    negative_index_row, positive_index_row = get_indeces_circular(pos, neighbor_distance)
 
     return negative_index_row, positive_index_row
 
@@ -90,8 +89,7 @@ def neighborhood(winner_position, length, neighbor_distance, task):
         for i in range(negative_index, positive_index):
             neighbors.append([0, i])
     elif task == "task4_2":
-        negative_index_row, positive_index_row = get_indeces_2dim(
-            winner_position, length, neighbor_distance)
+        negative_index_row, positive_index_row = get_indeces_2dim(winner_position, neighbor_distance)
         for i in range(negative_index_row, positive_index_row + 1):
             # for j in range(negative_index_col, positive_index_col):
             neighbors.append([0, i])
@@ -112,20 +110,22 @@ def weight_update(neighbors, x, weight):
 
 
 def som_algorithm(weight, data, task):
+    real_distance = 0
+    neighbor_distance = 0
     if task == "task4_1":
         neighbor_distance = NEIGHBOR_DISTANCE_ANIMAL
         real_distance = NEIGHBOR_DISTANCE_ANIMAL
     elif task == "task4_2":
         neighbor_distance = NEIGHBOR_DISTANCE_CYCLING
-    
+
     for i in range(EPOCH):
         for j in range(data.shape[0]):
             win_pos = similarity(data[j, :], weight)
             neighbors = neighborhood(win_pos, weight.shape[0], neighbor_distance, task)
             weight = weight_update(neighbors, data[j, :], weight)
-        
         if task == "task4_1":
-            real_distance -= (NEIGHBOR_DISTANCE_ANIMAL - 1) / EPOCH  # This is the real distance without roundning (always updates)
+            real_distance -= (NEIGHBOR_DISTANCE_ANIMAL - 1) / EPOCH  # This is the real distance without roundning (
+            # always updates)
             neighbor_distance = round(real_distance)  # Rounding the real distance for iterations
         elif task == "task4_2":
             if i < EPOCH / 2:
@@ -136,27 +136,31 @@ def som_algorithm(weight, data, task):
     return weight
 
 
-def sorting(weight, data, task,names=[]):
+def sorting(weight, data, task, names=None):
+    if names is None:
+        names = []
     best_combination = list()
     for j in range(data.shape[0]):
         win_pos = similarity(data[j, :], weight)
-        best_combination.append(win_pos)        
+        best_combination.append(win_pos)
 
     if task == "task4_1":
         data = {'Animal': names, 'WeightIndex': best_combination}  # Generate data structure
     elif task == "task4_2":
         data = {'City': range(1, data.shape[0] + 1), 'WeightIndex': best_combination}
-    
+
     data = pd.DataFrame(data=data)  # Generating Panda's DataFrame
     data = data.sort_values(by='WeightIndex')  # Sorting DataFrame by values of the best position ['col2']
 
     return data
+
 
 """
 ##############################################################################
 # Task 1
 ###############################################################################
 """
+
 
 def task4_1(filename, weight, task):
     animal_names, animals_dat, animals_att = read_data(filename, task)
@@ -177,7 +181,7 @@ def connectpoints(x, y, p1, p2, i):
     y1, y2 = y[p1], y[p2]
     x_mean = min(x1, x2) + (max(x1, x2) - min(x1, x2)) / 2
     y_mean = min(y1, y2) + (max(y1, y2) - min(y1, y2)) / 2
-    plt.plot([x1, x2], [y1, y2], 'k-', c=np.random.rand(3,))
+    plt.plot([x1, x2], [y1, y2], 'k-', c=np.random.rand(3, ))
     plt.annotate(str(i), xy=(x_mean, y_mean))
 
 
@@ -190,13 +194,20 @@ def plotting(dat, sorted_cities):
     plt.plot(data[:, 0], data[:, 1], 'ro')
     for i in range(data.shape[0]):
         ax.annotate("City" + str(i + 1), (data[i, 0], data[i, 1]))
-    for i in range(sorted_cities.shape[0] - 1):
-        connectpoints(data[:, 0], data[:, 1], sorted_cities[i, 0] - 1, sorted_cities[i + 1, 0] - 1, i + 1)
+    for i in range(sorted_cities.shape[0]):
+        if i == sorted_cities.shape[0] - 1:
+            connectpoints(data[:, 0], data[:, 1], sorted_cities[i, 0] - 1, sorted_cities[0, 0] - 1, i + 1)
+        else:
+            connectpoints(data[:, 0], data[:, 1], sorted_cities[i, 0] - 1, sorted_cities[i + 1, 0] - 1, i + 1)
     plt.xlabel("$X_{axis}$")
     plt.ylabel("$Y_{axis}$")
     plt.title("Path across cities")
+    axes = plt.gca()
+    axes.set_xlim([0, 1])
+    axes.set_ylim([0, 1])
+    plt.grid()
     plt.show()
-    
+
 
 def task4_2(filename, weight, task):
     cities, data = read_data(filename, task)
@@ -204,7 +215,6 @@ def task4_2(filename, weight, task):
     sorted_cities = sorting(weight_updated, cities, task)
     sorted_cities = sorted_cities.to_numpy()
     plotting(data, sorted_cities)
-    
 
 
 """
@@ -220,5 +230,3 @@ def task4_3(filename, weight, task):
     party = pd.read_csv(filename[2], header=None)
     names = pd.read_csv(filename[3], header=None, encoding='utf_8')
     district = pd.read_csv(filename[4], header=None)
-    
-
--- Assignment2/Part2.py
+++ Assignment2/Part2.py
@@ -2,14 +2,9 @@ import numpy as np
 import Algorithms_part2 as alg
 
 # TASK 4.1 - animal sorting
-
-#assignment_task = "task4_1"  
-"""
 filenames = ["data_lab2/animalnames.txt", "data_lab2/animals.dat", "data_lab2/animalattributes.txt"]
 weight = np.random.rand(100, 84)
 alg.task4_1(filenames, weight, task="task4_1")
-"""
-
 
 # TASK 4.2 - cyclic tour
 filenames = ["data_lab2/cities.dat"]
@@ -23,4 +18,3 @@ filenames = ["data_lab2/votes.dat", "data_lab2/mpsex.dat", "data_lab2/mpparty.da
 weight = np.random.rand(10, 10)
 alg.task4_3(filenames, weight, task="task4_3")
 """
-
