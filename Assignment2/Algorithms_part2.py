0679da5521720f94943791813c5a01505dd98008
Lucasqgongora <lucas.q.gongora@gmail.com>
2020-01-30 11:12:14 +0100

    starting task 4.3

--- Assignment2/Algorithms_part2.py
+++ Assignment2/Algorithms_part2.py
@@ -4,19 +4,37 @@ import matplotlib.pyplot as plt
 
 ETA = 0.2
 EPOCH = 20
-NEIGHBOR_DISTANCE = 2
+NEIGHBOR_DISTANCE_ANIMAL = 25
+NEIGHBOR_DISTANCE_CYCLING = 2
 
 
-def read_data(filename):
-    animal_names = pd.read_csv(filename[0], header=None)
-    animal_names = clean_names(pd.DataFrame(animal_names).iloc[:, 0].tolist())
-    animals_dat = pd.read_csv(filename[1], header=None)
-    animals_dat = pd.DataFrame(animals_dat).iloc[0, :].tolist()
-    animals_att = pd.read_csv(filename[2], header=None)
-    animals_att = pd.DataFrame(animals_att).iloc[:, 0].tolist()
-    animals_dat = np.array(animals_dat).reshape((len(animal_names), len(animals_att)))
 
-    return animal_names, animals_dat, animals_att
+"""
+##############################################################################
+# Functions for both tasks
+###############################################################################
+"""
+
+def read_data(filename, task):
+    if task == "task4_1":
+        animal_names = pd.read_csv(filename[0], header=None)
+        animal_names = clean_names(pd.DataFrame(animal_names).iloc[:, 0].tolist())
+        animals_dat = pd.read_csv(filename[1], header=None)
+        animals_dat = pd.DataFrame(animals_dat).iloc[0, :].tolist()
+        animals_att = pd.read_csv(filename[2], header=None)
+        animals_att = pd.DataFrame(animals_att).iloc[:, 0].tolist()
+        animals_dat = np.array(animals_dat).reshape((len(animal_names), len(animals_att)))
+
+        return animal_names, animals_dat, animals_att
+    
+    if task == "task4_2":
+        data = pd.read_csv(filename[0], header=None)
+        data[1].replace(regex=True, inplace=True, to_replace=r';', value=r'')
+        cities = data.to_numpy()
+        cities[:, 0] = [float(i) for i in cities[:, 0]]
+        cities[:, 1] = [float(i) for i in cities[:, 1]]
+        
+        return cities, data
 
 
 def clean_names(names):
@@ -93,59 +111,65 @@ def weight_update(neighbors, x, weight):
     return weight
 
 
-def som_task4_2(weight, data, task):
-    neighbor_distance = NEIGHBOR_DISTANCE
+def som_algorithm(weight, data, task):
+    if task == "task4_1":
+        neighbor_distance = NEIGHBOR_DISTANCE_ANIMAL
+        real_distance = NEIGHBOR_DISTANCE_ANIMAL
+    elif task == "task4_2":
+        neighbor_distance = NEIGHBOR_DISTANCE_CYCLING
+    
     for i in range(EPOCH):
         for j in range(data.shape[0]):
             win_pos = similarity(data[j, :], weight)
             neighbors = neighborhood(win_pos, weight.shape[0], neighbor_distance, task)
             weight = weight_update(neighbors, data[j, :], weight)
-        if i < EPOCH / 2:
-            neighbor_distance = 1
-            if i == EPOCH - 1:
-                neighbor_distance = 0
+        
+        if task == "task4_1":
+            real_distance -= (NEIGHBOR_DISTANCE_ANIMAL - 1) / EPOCH  # This is the real distance without roundning (always updates)
+            neighbor_distance = round(real_distance)  # Rounding the real distance for iterations
+        elif task == "task4_2":
+            if i < EPOCH / 2:
+                neighbor_distance = 1
+                if i == EPOCH - 1:
+                    neighbor_distance = 0
 
     return weight
 
 
-def som_algorithm(weight, animal_names, animals_dat, task):
-    neighbor_distance = NEIGHBOR_DISTANCE
-    real_distance = NEIGHBOR_DISTANCE
-    for i in range(EPOCH):
-        for j in range(len(animal_names)):
-            win_pos = similarity(animals_dat[j, :], weight)
-            neighbors = neighborhood(win_pos, weight.shape[0], neighbor_distance, task)
-            weight = weight_update(neighbors, animals_dat[j, :], weight)
-        real_distance -= (NEIGHBOR_DISTANCE - 1) / EPOCH  # This is the real distance without roundning (always updates)
-        neighbor_distance = round(real_distance)  # Rounding the real distance for iterations
-
-    return weight
-
-
-def animals_sorting(weight, animals_dat, animal_names):
+def sorting(weight, data, task,names=[]):
     best_combination = list()
-    for j in range(len(animal_names)):
-        win_pos = similarity(animals_dat[j, :], weight)
-        best_combination.append(win_pos)
+    for j in range(data.shape[0]):
+        win_pos = similarity(data[j, :], weight)
+        best_combination.append(win_pos)        
 
-    data = {'Animal': animal_names, 'WeightIndex': best_combination}  # Generate data structure
+    if task == "task4_1":
+        data = {'Animal': names, 'WeightIndex': best_combination}  # Generate data structure
+    elif task == "task4_2":
+        data = {'City': range(1, data.shape[0] + 1), 'WeightIndex': best_combination}
+    
     data = pd.DataFrame(data=data)  # Generating Panda's DataFrame
     data = data.sort_values(by='WeightIndex')  # Sorting DataFrame by values of the best position ['col2']
 
     return data
 
+"""
+##############################################################################
+# Task 1
+###############################################################################
+"""
 
-def cities_sorting(weight, cities):
-    best_combination = list()
-    for j in range(cities.shape[0]):
-        win_pos = similarity(cities[j, :], weight)
-        best_combination.append(win_pos)
+def task4_1(filename, weight, task):
+    animal_names, animals_dat, animals_att = read_data(filename, task)
+    weight_updated = som_algorithm(weight, animals_dat, task)
+    sorted_animals = sorting(weight_updated, animals_dat, task, animal_names)
+    print(sorted_animals)
 
-    data = {'City': range(1, cities.shape[0] + 1), 'WeightIndex': best_combination}  # Generate data structure
-    data = pd.DataFrame(data=data)  # Generating Panda's DataFrame
-    data = data.sort_values(by='WeightIndex')  # Sorting DataFrame by values of the best position ['col2']
 
-    return data
+"""
+##############################################################################
+# Task 2
+###############################################################################
+"""
 
 
 def connectpoints(x, y, p1, p2, i):
@@ -157,23 +181,8 @@ def connectpoints(x, y, p1, p2, i):
     plt.annotate(str(i), xy=(x_mean, y_mean))
 
 
-def task4_1(filename, weight, task):
-    animal_names, animals_dat, animals_att = read_data(filename)
-    weight_updated = som_algorithm(weight, animal_names, animals_dat, task)
-    sorted_animals = animals_sorting(weight_updated, animals_dat, animal_names)
-    print(sorted_animals)
-
-
-def task4_2(filename, weight, task):
-    data = pd.read_csv(filename[0], header=None)
-    data[1].replace(regex=True, inplace=True, to_replace=r';', value=r'')
-    cities = data.to_numpy()
-    cities[:, 0] = [float(i) for i in cities[:, 0]]
-    cities[:, 1] = [float(i) for i in cities[:, 1]]
-    weight_updated = som_task4_2(weight, cities, task)
-    sorted_cities = cities_sorting(weight_updated, cities)
-    sorted_cities = sorted_cities.to_numpy()
-    data = data.to_numpy()
+def plotting(dat, sorted_cities):
+    data = dat.to_numpy()
     fig = plt.figure()
     ax = fig.add_subplot(111)
     data[:, 0] = [float(i) for i in data[:, 0]]
@@ -187,3 +196,29 @@ def task4_2(filename, weight, task):
     plt.ylabel("$Y_{axis}$")
     plt.title("Path across cities")
     plt.show()
+    
+
+def task4_2(filename, weight, task):
+    cities, data = read_data(filename, task)
+    weight_updated = som_algorithm(weight, cities, task)
+    sorted_cities = sorting(weight_updated, cities, task)
+    sorted_cities = sorted_cities.to_numpy()
+    plotting(data, sorted_cities)
+    
+
+
+"""
+##############################################################################
+# Task 3
+###############################################################################
+"""
+
+
+def task4_3(filename, weight, task):
+    votes = pd.read_csv(filename[0], header=None)
+    sex = pd.read_csv(filename[1], header=None)
+    party = pd.read_csv(filename[2], header=None)
+    names = pd.read_csv(filename[3], header=None, encoding='utf_8')
+    district = pd.read_csv(filename[4], header=None)
+    
+
--- Assignment2/Part2.py
+++ Assignment2/Part2.py
@@ -2,13 +2,25 @@ import numpy as np
 import Algorithms_part2 as alg
 
 # TASK 4.1 - animal sorting
+
+#assignment_task = "task4_1"  
 """
 filenames = ["data_lab2/animalnames.txt", "data_lab2/animals.dat", "data_lab2/animalattributes.txt"]
 weight = np.random.rand(100, 84)
 alg.task4_1(filenames, weight, task="task4_1")
 """
 
+
 # TASK 4.2 - cyclic tour
 filenames = ["data_lab2/cities.dat"]
 weight = np.random.rand(10, 2)
 alg.task4_2(filenames, weight, task="task4_2")
+
+"""
+# TASK 4.3 - cyclic tour
+filenames = ["data_lab2/votes.dat", "data_lab2/mpsex.dat", "data_lab2/mpparty.dat", \
+    "data_lab2/mpnames.txt", "data_lab2/mpdistrict.dat"]
+weight = np.random.rand(10, 10)
+alg.task4_3(filenames, weight, task="task4_3")
+"""
+
