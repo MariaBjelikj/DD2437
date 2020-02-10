87c11d484cb287cf53db4585d15d1ad5cdbebb7b
Maria Bjelikj <bjelikm@gmail.com>
2020-02-10 20:47:07 +0100

    .

--- Assignment3/3_2.py
+++ Assignment3/3_2.py
@@ -1,19 +1,26 @@
 from Hopfield_Network import *
+import numpy as np
 
 # Load data
 data = np.loadtxt('pict.dat', delimiter=",", dtype=int).reshape(-1, 1024)
 
-# First train for first 3 images
-x_train = data[:3, :].copy()
-w = weights(x_train)
+# Learn the first 3 patterns
+x = data[:3, :].copy()
+w = weights(x)
 
-# Test for next 2 images
-x_test = data[5:10, :].copy()
-x_test_1 = data[8:10, :].copy()
-#prediction = update_syncroniously(x_test, w)
-#prediction_1 = update_asyncroniously(x_test, w)
+"""prediction = recall(data[9:11, :], w, update_type="asynchronous")
 
-prediction = recall(x_test, w, update_type="asynchronous")
-
+print("Trying to complete degraded pattern p10.")
+display(data[9:10, :])
 display(prediction[0])
-#display(prediction_1[0])
+# This degraded pattern converges to stable pattern p1 (pattern data[0])
+#display(data[0])
+
+print("Trying to complate degraded pattern p11.")
+display(data[10:11, :])
+display(prediction[1])"""
+
+
+# Random asynchronous (uncomment code for 3.2 in Hopfield_Network)
+display(data[10])
+recall(data[10:11, :].copy(), w, update_type="asynchronous", asyn_type="random")
--- Assignment3/Hopfield_Network.py
+++ Assignment3/Hopfield_Network.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ITERATIONS = 100  # number of iterations for syncronious update
+ITERATIONS = 2000  # number of iterations for syncronious update
 
 
 def generate_data(d_type):
@@ -26,7 +26,6 @@ def generate_data(d_type):
         x3d = [1, -1, -1, 1, -1, 1, -1, -1]
         return np.vstack([x1d, x2d, x3d])
 
-
 def weights(x):
     # Update weights for Little Model
     n = x.shape[0]  # number of patterns
@@ -44,7 +43,6 @@ def weights(x):
     # w = w / M # normalize, only if bias is used
     return w
 
-
 def set_sign(x, w, index):
     x_sign = np.copy(x)
     for i in range(len(x[index, :])):
@@ -57,8 +55,15 @@ def set_sign(x, w, index):
             x_sign[index, i] = -1
     return x_sign[index, :]
 
-
-def recall(x, w, update_type="synchronous", convergence_type=False):
+def recall(x, w, update_type="synchronous", convergence_type=False, asyn_type=False):
+    """ 
+    PARAMETERS:
+    # update_type: can be "synchronous" or "asynchronous"
+    # convergence_type: choose "energy" for task 3.3 and above
+    # asyn_type: type of asynchronous update, "random" or sequential by default
+        
+    """
+    
     if update_type == "synchronous":
         # Update the weights synchronously, aka "Little Model"
         x_current = np.copy(x)
@@ -87,7 +92,7 @@ def recall(x, w, update_type="synchronous", convergence_type=False):
                 E_current = np.copy(E_new)
             
             else: 
-                if np.all(x_new == x_current.T):  # check recall
+                if np.all(x_new == x_current):  # check recall
                     print("The network converged after {} iterations.".format(iteration))
                     break  # the state is stable (convergence, break loop)
                 
@@ -101,13 +106,20 @@ def recall(x, w, update_type="synchronous", convergence_type=False):
         # Compute energy of the initial state
         E_current = energy(x, w)
         convergence_count = 0
- 
+        indexes = []
+        
         # Iterate for convergence
         for iteration in range(ITERATIONS):
+            #indexes = []
             for i in range(x.shape[0]):
                 for j in range(x.shape[1]):
-                    idx = np.random.randint(0, x.shape[1])
-                    x_new[i, idx] = np.sign(x_current[i, :] @ w[idx])
+                    if asyn_type == "random":
+                        idx = np.random.randint(0, x.shape[1])
+                        #if idx not in indexes: 
+                            #indexes.append(idx) # so we update the indexes only once
+                        x_new[i, idx] = np.where((x_current[i, :] @ w[idx]) >= 0, 1, -1)
+                    else: x_new[i, j] = np.where((x_current[i, :] @ w[j]) >= 0, 1, -1)
+
                     
             if convergence_type == "energy":
                 E_new = energy(x_new, w)
@@ -118,15 +130,24 @@ def recall(x, w, update_type="synchronous", convergence_type=False):
                 if convergence_count > 5:
                     print("The network converged after {} iterations.".format(iteration))
                     break
-                    
+                
                 E_current = np.copy(E_new)
     
-            else:
-                if np.all(x_new == x_current.T):  # check recall
+            #else:
+                if np.all(x_new == x_current):  # check recall
                     print("The network converged after {} iterations.".format(iteration))
                     break  # the state is stable (convergence, break loop)
                     
             x_current = np.copy(x_new)
+            
+            # Task 3.2, plot every 100th iteration or so
+            # to use this, comment out the parts for convergence so the network goes through all the iterations
+            iters = [200, 500, 1000, 1500, 2000, 2500, 2900]
+            if iteration in iters:
+                display(x_current, "Recall after {} iterations.".format(iteration))
+                
+            
+            
                           
     return x_current
 
@@ -140,15 +161,16 @@ def find_attractors(data, data_updated, w):
     
     return attractors
         
-
 def energy(state, w):
-    return np.sum(- 0.5 * state @ w @ state.T)
+    return np.sum(- state @ w @ state.T)
 
-def display(image):
+def display(image, title=False):
     # For task 3.2
 
-    # display images in shape (32, 32)
-    plt.imshow(image.reshape(32, 32), interpolation="nearest")
+    # display images in shape (32, 32) and rotate them so face is up
+    plt.imshow(np.rot90(image.reshape(32, 32)), origin='lower', interpolation="nearest")
+    if title:
+        plt.title(title)
     plt.show()
     
     
