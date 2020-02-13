4c0dc310dad3ed442c4921fa2a56833c98118883
fernando2393 <garciasanzfernando2393@gmail.com>
2020-02-13 16:47:05 +0100

    3_4 finished and checked. Continuing in 3_5. Noised images function created

--- Assignment3/3_4.py
+++ Assignment3/3_4.py
@@ -3,31 +3,18 @@ from sklearn.utils import shuffle
 import numpy as np
 import seaborn as sns
 
-PERCENTAGES = np.linspace(start = 0, stop = 1, num = 11)
+PERCENTAGES = np.linspace(start=0, stop=1, num=11)
 ITERATIONS = 100
 
 # Load data
 data = np.loadtxt('pict.dat', delimiter=",", dtype=int).reshape(-1, 1024)
 
-x = data[:3, :].copy()
-w = weights(x)
-
-image = 0   
-
+w = weights(data[:3, :])
+image = 0
 counter = np.zeros(len(PERCENTAGES))
-for i,perc in enumerate(PERCENTAGES):
-    for _ in range(ITERATIONS):
-        noised_data = np.copy(data)
-        indexes = shuffle(np.arange(0, noised_data.shape[1]))
-        for k in range(int(perc * noised_data.shape[1])):
-            noised_data[image, indexes[k]] = -noised_data[image, indexes[k]]
-        x_current = recall(noised_data[image:(image+1), :], w, update_type="synchronous", convergence_type='energy')
-        if np.all(x_current == data[image]):
-            counter[i] += 1
-
-
+counter = noised_images(PERCENTAGES, data, image, counter, w)
 plt.figure()
-#sns.set_style('darkgrid')
+# sns.set_style('darkgrid')
 counter_plot = sns.lineplot(x = PERCENTAGES, y=counter)
 counter_plot.set(xlabel='Noise level', ylabel='Accuracy')
 plt.show()
--- Assignment3/3_5.py
+++ Assignment3/3_5.py
@@ -1,36 +1,25 @@
 from Hopfield_Network import *
-from sklearn.utils import shuffle
 import numpy as np
 import seaborn as sns
 
-PERCENTAGES = np.linspace(start = 0, stop = 1, num = 11)
+PERCENTAGES = np.linspace(start=0, stop=1, num=11)
 ITERATIONS = 100
 
 # Load data
 data = np.loadtxt('pict.dat', delimiter=",", dtype=int).reshape(-1, 1024)
 
-x = data[:4, :].copy()
-w = weights(x)
-
-image = 0   
-
-counter = np.zeros(len(PERCENTAGES))
-for i,perc in enumerate(PERCENTAGES):
-    for _ in range(ITERATIONS):
-        noised_data = np.copy(data)
-        indexes = shuffle(np.arange(0, noised_data.shape[1]))
-        for k in range(int(perc * noised_data.shape[1])):
-            noised_data[image, indexes[k]] = -noised_data[image, indexes[k]]
-        x_current = recall(noised_data[image:(image+1), :], w, update_type="synchronous", convergence_type='energy')
-        if np.all(x_current == data[image]):
-            counter[i] += 1
-
-
-plt.figure()
-#sns.set_style('darkgrid')
-counter_plot = sns.lineplot(x = PERCENTAGES, y=counter)
-counter_plot.set(xlabel='Noise level', ylabel='Accuracy')
-plt.show()
+for pat in range(4, 8):
+    print("Running network for image", pat)
+    w = weights(data[:pat, :])
+    image = 0
+    counter = np.zeros(len(PERCENTAGES))
+    counter = noised_images(PERCENTAGES, data, image, counter, w)
+    plt.figure()
+    # sns.set_style('darkgrid')
+    counter_plot = sns.lineplot(x=PERCENTAGES, y=counter)
+    counter_plot.set(xlabel='Noise level', ylabel='Accuracy')
+    plt.show()
+    input("Press enter to continue...")
 
 # noised_filename = 'images/image{}_error{}_noised'.format(image, i)
 # noised_filename = noised_filename.replace('.', ',', 1)
