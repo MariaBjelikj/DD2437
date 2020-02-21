a09e31bcb78e923a01450be5ba9542d164219e2a
Maria Bjelikj <bjelikm@gmail.com>
2020-02-21 15:25:02 +0100

    Update rbm.py

--- Assignment4/code/rbm.py
+++ Assignment4/code/rbm.py
@@ -330,12 +330,13 @@ class RestrictedBoltzmannMachine:
            all args have shape (size of mini-batch, size of respective layer)
         """
 
+        # DONE
         # [TODO TASK 4.3] find the gradients from the arguments (replace the 0s below)
         #  and update the weight and bias parameters.
 
-        self.delta_weight_h_to_v += 0
-        self.delta_bias_v += 0
-
+        self.delta_weight_h_to_v = self.learning_rate * inps.T @ (trgs - preds)
+        self.delta_bias_v = self.learning_rate * (np.sum(trgs - preds, axis=0)) 
+        
         self.weight_h_to_v += self.delta_weight_h_to_v
         self.bias_v += self.delta_bias_v
 
